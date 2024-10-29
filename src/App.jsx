18f3660b74e92bdf2feeb1e03d65b9ba13b7d73e
Nirmal-wishwantha <nirmalwishwantha@gmail.com>
2024-10-30 03:03:37 +0530

    register api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
+import { Route, Router, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './pages/login/Login'
 import Register from './pages/register/Register'
 
+import { Toast } from './common/funtion/index';
+import Home from './pages/home/Home';
+import { useState } from 'react';
+
 
 function App() {
 
+  const [login, setLogin] = useState(false);
+
   return (
-  
+
     <div>
-      {/* <Login/> */}
-      {/* <Register/> */}
+
+      {/* {
+        <Login /> ? <Main /> :
+
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Toast />
+          </Routes>
+
+      } */}
+
+
+      <Login/>
+
     </div>
   )
 }
 
 export default App
+
+
+
+function Main() {
+
+  return (
+
+    <div>
+      <Home />
+    </div>
+  )
+}
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, TextField, Typography, Paper, Avatar, Link } from '@mui/material';
-import { PersonAdd } from '@mui/icons-material';
+import { Email, Password, PersonAdd } from '@mui/icons-material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import instance from '../../services/AxiosOder';
+import { toast } from 'react-toastify'; 
+
 
 const theme = createTheme({
   palette: {
@@ -24,6 +27,61 @@ const theme = createTheme({
 });
 
 export default function Register() {
+
+  const navigation = useNavigate();
+
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  // const [name,setName]=useState();
+
+
+  const userRegister = () => {
+
+    const data = {
+      name: name,
+      email: email,
+      password: password
+    }
+
+    instance.post('/api/v1/user/register', data)
+      .then((res) => {
+        console.log(res);
+
+
+        toast.success("Registration Successful! You have registered successfully.", {
+          position: "top-center",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: true,
+          draggable: true,
+          pauseOnHover: true,
+        });
+
+        setTimeout(() => {
+          navigation('/login');
+        }, 2000);
+      })
+
+      .catch((err) => {
+        console.log(err);
+
+        toast.error("Registration Failed! Please try again later.", {
+          position: "top-center",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: true,
+          draggable: true,
+          pauseOnHover: true,
+        });
+
+
+
+      })
+  }
+
+
   return (
     <ThemeProvider theme={theme}>
       <Box
@@ -54,13 +112,16 @@ export default function Register() {
           <Typography variant="h5" align="center" gutterBottom>
             Register
           </Typography>
+
           <TextField
             label="Username"
             variant="outlined"
             fullWidth
             margin="normal"
             sx={{ '& .MuiOutlinedInput-root': { color: 'text.primary' } }}
+            onChange={(e) => setName(e.target.value)}
           />
+
           <TextField
             label="Email"
             type="email"
@@ -68,7 +129,9 @@ export default function Register() {
             fullWidth
             margin="normal"
             sx={{ '& .MuiOutlinedInput-root': { color: 'text.primary' } }}
+            onChange={(e) => setEmail(e.target.value)}
           />
+
           <TextField
             label="Password"
             type="password"
@@ -76,7 +139,9 @@ export default function Register() {
             fullWidth
             margin="normal"
             sx={{ '& .MuiOutlinedInput-root': { color: 'text.primary' } }}
+            onChange={(e) => setPassword(e.target.value)}
           />
+
           <TextField
             label="Confirm Password"
             type="password"
@@ -85,14 +150,18 @@ export default function Register() {
             margin="normal"
             sx={{ '& .MuiOutlinedInput-root': { color: 'text.primary' } }}
           />
+
           <Button
             variant="contained"
             color="primary"
             fullWidth
             sx={{ mt: 2, animation: 'pulse 2s infinite' }}
+            onClick={userRegister}
           >
             Register
           </Button>
+
+
           <Typography align="center" mt={2}>
             Already registered?{' '}
             <Link component={RouterLink} to="/login" color="primary">
